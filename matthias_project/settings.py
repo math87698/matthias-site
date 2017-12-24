9edf47b1d100e2e45c020a911799c4230dab611d
matthiashugli <development@keepertrainer.ch>
2017-12-24 01:35:46 +0100

    added allowed host and debug to false

--- matthias_project/settings.py
+++ matthias_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5nci63k&-v*p0gtv5sn!7!qvw_z!15!lcqbb%++o6edngp_jyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matthias-site.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
