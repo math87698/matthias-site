05cfa745c516ab81cf066c6f9cb5dd8eedb67606
matthiashugli <development@keepertrainer.ch>
2017-12-27 13:42:15 +0100

    removed position and education models because of heroku migrations issues

--- website/models.py
+++ website/models.py
@@ -4,19 +4,3 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Position(models.Model):
-    company = models.CharField(max_length=70)
-    city = models.CharField(max_length=70)
-    country = models.CharField(max_length=70)
-    date_from = models.DateField()
-    date_to = models.DateField(blank=True, null=True)
-    title = models.CharField(max_length=150)
-
-
-class Education(models.Model):
-    facility = models.CharField(max_length=70)
-    city = models.CharField(max_length=70)
-    country = models.CharField(max_length=70)
-    date_from = models.DateField()
-    date_to = models.DateField()
-    degree = models.CharField(max_length=70)
--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from .models import Position
+#from .models import Position
 
 
 def index(request):
-    position = get_object_or_404(Position)
-    context = {
-        'position': position
-    }
+    # position = get_object_or_404(Position)
+    # context = {
+    #     'position': position
+    # }
     return render(request, 'base.html', context)
