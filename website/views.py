abb001d0ffa62e9723fbcec6f096e85f9133f680
matthiashugli <development@keepertrainer.ch>
2017-12-31 00:37:37 +0100

    refactored footer

--- website/views.py
+++ website/views.py
@@ -2,12 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from .models import Position
+from .models import Position, Education
 
 
 def index(request):
     positions = Position.objects.all()
+    educations = Education.objects.all()
     context = {
-        'positions': positions
+        'positions': positions,
+        'educations': educations,
     }
     return render(request, 'base.html', context)
+
+
+def imprint(request):
+    return render(request, 'imprint.html')
