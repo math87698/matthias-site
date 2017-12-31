d4e7ecb14e609cbd85ff083a94e12b19a155aed4
matthiashugli <development@keepertrainer.ch>
2017-12-31 01:41:34 +0100

    fixed missing hosts, changed debug to false

--- matthias_project/settings.py
+++ matthias_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '5nci63k&-v*p0gtv5sn!7!qvw_z!15!lcqbb%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['matthias-site.herokuapp.com', 'localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
